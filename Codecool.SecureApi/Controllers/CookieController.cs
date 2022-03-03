e33622489173228edfafa38bba8ca7303785d6da
bartosz.rzemek <bartosz.rzemek@codecool.com>
2022-03-03 21:29:22 +0100

    Initial commit

---
+++ Codecool.SecureApi/Controllers/CookieController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codecool.SecureApi.Controllers
+{
+    public class LoginData
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class CookieController : Controller
+    {
+        
+        private IServiceProvider Services { get; set; }
+        private HttpContext Context { get { return Services.GetRequiredService<IHttpContextAccessor>().HttpContext; } }
+        public CookieController(IServiceProvider services)
+        {
+            this.Services = services;
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginData loginData)
+        {
+            if (loginData.login == "juzek" && loginData.password == "maslo")
+            {
+                Context.Response.Cookies.Append("IsAuthenticated", "1");
+                return Ok(loginData.login);
+
+            }
+            else
+            {
+                Context.Response.Cookies.Append("IsAuthenticated", "0");
+                
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            }
+        }
+    }
+}
---
+++ Codecool.SecureApi/Controllers/SecureController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codecool.SecureApi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class SecureController : Controller
+    {
+        
+        private IServiceProvider Services { get; set; }
+        private Microsoft.AspNetCore.Antiforgery.IAntiforgery Csrf { get { return Services.GetRequiredService<Microsoft.AspNetCore.Antiforgery.IAntiforgery>(); } }
+
+        private HttpContext Context { get { return Services.GetRequiredService<IHttpContextAccessor>().HttpContext; } }
+        public SecureController(IServiceProvider services)
+        {
+            this.Services = services;
+        }
+
+
+        [HttpPost]
+        public IActionResult Login(LoginData loginData)
+        {
+            if (loginData.login == "juzek" && loginData.password == "maslo")
+            {
+                var tokens = Csrf.GetAndStoreTokens(HttpContext);
+                Response.Cookies.Append("X-XSRF-TOKEN", tokens.RequestToken, new Microsoft.AspNetCore.Http.CookieOptions
+                {
+                    HttpOnly = false
+                });
+
+            }
+            else
+            {
+                return BadRequest(new { message = "Username or password is incorrect" });
+            }
+            
+            return Ok(loginData.login);
+        }
+    }
+}
